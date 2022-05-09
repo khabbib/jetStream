b0bb7066baf3b09aec1deed83f5750cdb4332329
Kasper Kadestål <84015423+Kasperkadestal@users.noreply.github.com>
2022-05-09 10:16:37 +0200

    Quick fix
    Coordinates and language.

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.util.Objects;
