ed517ea9929fcedf746ac986b18842ebf737f5d7
SossioG <100341750+SossioG@users.noreply.github.com>
2022-05-12 15:12:40 +0200

    Registration repeatition solved

--- src/main/java/application/Components/RegistrationUser.java
+++ src/main/java/application/Components/RegistrationUser.java
@@ -10,7 +10,7 @@ import javafx.scene.control.Label;
 import java.sql.SQLException;
 
 /**
- * This class registers users and handles errors. Only users!
+ * This class registers only users and handles errors.
  */
 public class RegistrationUser {
     private Controller controller;
@@ -20,9 +20,10 @@ public class RegistrationUser {
     private ConfirmActions confirmActions;
 
     /**
-     * @param controller
-     * @param connection
-     * @param config
+     * Class constructor.
+     * @param controller connects variables and methods.
+     * @param connection connects to database.
+     * @param config connects to root.
      */
     public RegistrationUser(Controller controller, Connection connection, Config config){
         this.controller = controller;
@@ -32,63 +33,67 @@ public class RegistrationUser {
     }
 
     /**
+     * This method registers a user to database.
+     * It checks if all text fields is verified with its specific limitation.
      * @return boolean.
      * @throws SQLException if any sql issue occur.
      * @author Khabib and Sossio. Developed by Sossio.
      */
     public boolean registerUser() {
         boolean registered = false;
-        if (!controller.registration_first_name.getText().isEmpty() && !controller.registration_last_name.getText().isEmpty() && !controller.registration_address.getText().isEmpty() && !controller.registration_email.getText().isEmpty() && !controller.registration_phone_number.getText().isEmpty() && !controller.registration_password.getText().isEmpty() && !controller.registration_confirm_password.getText().isEmpty()){
-            if ((controller.registration_first_name.getText().length() >= 3 && controller.registration_first_name.getText().length() <= 30)){
-                if ((controller.registration_last_name.getText().length() >= 3 && controller.registration_last_name.getText().length() <= 30)){
-                    if ((controller.registration_address.getText().length() >= 5 && controller.registration_address.getText().length() <= 60)){
-                        if((controller.registration_email.getText().length() >= 6 && controller.registration_email.getText().length() <= 30)){
-                            if ((controller.registration_phone_number.getText().length() == 10)){
-                                if (controller.registration_password.getText().length() >= 8 && controller.registration_password.getText().length() <= 20){
-                                    if (controller.registration_password.getText().equals(controller.registration_confirm_password.getText())){
-                                        if(controller.registration_email.getText().contains("@") && (controller.registration_email.getText().contains("gmail") || controller.registration_email.getText().contains("hotmail") || controller.registration_email.getText().contains("yahoo") || controller.registration_email.getText().contains("outlook"))){
-                                            boolean ok = connection.saveUser(controller.registration_first_name.getText(), controller.registration_last_name.getText(), controller.registration_address.getText(), controller.registration_email.getText(), controller.registration_phone_number.getText(), controller.registration_password.getText(), false);
-                                            if (ok) {
-                                                try {
-                                                    connection.setProfilePicture("application/profiles/user.png", controller.registration_email.getText());
-                                                } catch (SQLException ex) {
-                                                    ex.printStackTrace();
-                                                }
-                                                registered = true;
-                                            } else {
-                                                confirmActions.displayMessage(controller.registration_error_lbl, "Not registered! (Email exists!)", true);
+        if (Controller.validateInputLimit(controller.registration_first_name, "firstname")){
+            if (Controller.validateInputLimit(controller.registration_last_name, "lastname")){
+                if (Controller.validateInputLimit(controller.registration_address, "address")){
+                    if(Controller.validateInputLimit(controller.registration_email, "email")){
+                        if (Controller.validateInputLimit(controller.registration_phone_number, "phone")){
+                            if (Controller.validateInputLimit(controller.registration_password, "password")){
+                                if (controller.registration_password.getText().equals(controller.registration_confirm_password.getText())){
+                                    if(Controller.validateInputLimit(controller.registration_email, "email-format")){
+                                        boolean ok = connection.saveUser(controller.registration_first_name.getText(), controller.registration_last_name.getText(), controller.registration_address.getText(), controller.registration_email.getText(), controller.registration_phone_number.getText(), controller.registration_password.getText(), false);
+                                        if (ok) {
+                                            try {
+                                                connection.setProfilePicture("application/profiles/user.png", controller.registration_email.getText());
+                                            } catch (SQLException ex) {
+                                                ex.printStackTrace();
                                             }
-                                        }else {
-                                            confirmActions.displayMessage(controller.email_issue_reg, "Type issue [email]", true);
+                                            registered = true;
+                                        } else {
+                                            confirmActions.displayMessage(controller.registration_error_lbl, "Not registered! (Email exists!)", true);
                                         }
                                     }else {
-                                        confirmActions.displayMessage(controller.confirm_password_issue_reg, "Mach issue!", true);
+                                        confirmActions.displayMessage(controller.email_issue_reg, "Type issue [email]", true);
                                     }
                                 }else {
-                                    confirmActions.displayMessage(controller.password_issue_reg, "Size issue 8-20", true);
+                                    confirmActions.displayMessage(controller.confirm_password_issue_reg, "Mach issue!", true);
                                 }
                             }else {
-                                confirmActions.displayMessage(controller.phone_number_issue_reg, "Size issue 10 digit", true);
+                                confirmActions.displayMessage(controller.password_issue_reg, "Size issue 8-20", true);
                             }
-                        } else {
-                            confirmActions.displayMessage(controller.email_issue_reg, "Size issue 6-30", true);
+                        }else {
+                            confirmActions.displayMessage(controller.phone_number_issue_reg, "Size issue 10 digit", true);
                         }
-                    }else {
-                        confirmActions.displayMessage(controller.address_issue_reg, "Size issue 5-60", true);
+                    } else {
+                        confirmActions.displayMessage(controller.email_issue_reg, "Size issue 6-30", true);
                     }
-                } else {
-                    confirmActions.displayMessage(controller.last_name_issue_reg, "Size issue 3-30", true);
+                }else {
+                    confirmActions.displayMessage(controller.address_issue_reg, "Size issue 5-60", true);
                 }
             } else {
-                confirmActions.displayMessage(controller.name_issue_reg, "Size issue 3-30", true);
+                confirmActions.displayMessage(controller.last_name_issue_reg, "Size issue 3-30", true);
             }
-        }else {
-            confirmActions.displayMessage(controller.registration_error_lbl, "Empty field issue", true);
+        } else {
+            confirmActions.displayMessage(controller.name_issue_reg, "Size issue 3-30", true);
         }
         return registered;
     }
 
-    public void registerUser(ActionEvent e, Controller controller) {
+    /**
+     * This method is called when register button is clicked.
+     * @param e handles an event on button.
+     * @param controller connetcs to variables and methods.
+     * @author Khabib. Developed by Sossio.
+     */
+    public void registerUserBtnAction(ActionEvent e, Controller controller) {
         boolean ok = registerUser();
         System.out.println("Kommer in i if-satsen!");
         if (ok){
